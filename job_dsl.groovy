7ca9bc39583910566ad10b4c1ab66b7d91dcd7d0
Tom Mendy <tom.mendy@epitech.eu>
2024-03-10 16:54:04 +0100

    [ADD] SEED jib in job_dsl.groovy

--- job_dsl.groovy
+++ job_dsl.groovy
@@ -3,11 +3,11 @@ folder('Tools') {
     description('Folder for miscellaneous tools.')
 }
 
-job("Tools/clone-repository") {
-    description("Job to clone a repository")
+job('Tools/clone-repository') {
+    description('Job to clone a repository')
     wrappers {
         preBuildCleanup { // Clean before build
-           includePattern('**/target/**')
+            includePattern('**/target/**')
             deleteDirectories()
             cleanupParameter('CLEANUP')
         }
@@ -15,27 +15,43 @@ job("Tools/clone-repository") {
     steps {
         shell('git clone ${GIT_REPOSITORY_URL}')
     }
-    // configure { project ->
-    //     project / 'triggers' / 'spec' / 'hudson.triggers.SCMTrigger' {
-    //         spec('* * * * *')
-    //     }
-    // }
 }
 
-// job("Tools/SEED") {
-//     description("Seed job to create other jobs")
-//     parameters {
-//         stringParam('GITHUB_NAME', '', 'GitHub repository owner/repo_name (e.g.: "EpitechIT31000/chocolatine")')
-//         stringParam('DISPLAY_NAME', '', 'Display name for the job')
-//     }
-//     steps {
-//         dsl {
-//             external('job_dsl.groovy')
-//         }
-//     }
-//     configure { project ->
-//         project / 'triggers' / 'spec' / 'hudson.triggers.SCMTrigger' {
-//             spec('')
-//         }
-//     }
-// }
+job('Tools/SEED') {
+    description('Seed job to create other jobs')
+    parameters {
+        stringParam('GITHUB_NAME', '', 'GitHub repository owner/repo_name (e.g.: "EpitechIT31000/chocolatine")')
+        stringParam('DISPLAY_NAME', '', 'Display name for the job')
+    }
+    steps {
+        dsl {
+            scriptText = '''
+job(DISPLAY_NAME) {
+    scm {
+        git {
+            remote {
+                github(GITHUB_NAME)
+            }
+        }
+    }
+    wrappers {
+        preBuildCleanup { // Clean before build
+            includePattern('**/target/**')
+            deleteDirectories()
+            cleanupParameter('CLEANUP')
+        }
+    }
+    steps {
+        shell('make fclean')
+        shell('make')
+        shell('make tests_run')
+        shell('make clean')
+    }
+    triggers {
+        scm('H/1 * * * *')
+    }
+}
+            '''
+        }
+    }
+}
